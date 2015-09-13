57217980b6389a44e2e2f3426f6a2f2522f2718c
Charlin_冯成林 <1761904945@qq.com>
2015-09-13 21:36:14 +0800

    KRVideoPlayerVC
    KRVideoPlayer 修改版

---
+++ KRVideoPlayerVC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KRVideoPlayerVC
+//
+//  Created by 成林 on 15/9/13.
+//  Copyright (c) 2015年 冯成林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
